1dfdeff84efa3209407d55f9d09c6d94d7ef2049
Arch0zz <104281088+scriptskali@users.noreply.github.com>
2022-05-03 21:06:30 -0400

    Create INFORMACION-OF-CREATOR.py

---
+++ INFORMACION-OF-CREATOR.py
@@ -0,0 +1,13 @@
+# A-R-C-H
+GREEN = '\033[32m'
+import os
+os.system("clear")
+print(GREEN+"============================")
+print(GREEN+"|     Created By Arch0zz   | ")
+print(GREEN+"============================")
+print(GREEN+"|     TIK TOK: arch0zz     |")
+print(GREEN+"============================")
+print(GREEN+"|    TELEGRAM: arch0zz     |")
+print(GREEN+"============================")
+print(GREEN+"|    LIMITE DE DOXXEO:100  |")
+print(GREEN+"============================")
